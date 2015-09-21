597391160dcb7336411e7ae3d318cbbc465c74ed
Frederick Jansen <frederick.jansen@gmail.com>
2015-09-21 16:03:35 -0400

    Abstracted large part of PowerWorld code Threaded methods can now be called with a decorator, gets rid of COM object boilerplate

---
+++ main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import timeit
+
+from powergrid.powergrid import PowerGrid
+# Python with COM requires the pyWin32 extensions
+from win32com.client import VARIANT
+
+import pythoncom
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+filename = current_dir + '\\powergrid\\resources\\sampleCase.pwb'
+pw = PowerGrid(8)
+
+# The following function will determine if any errors are returned and print an appropriate message.
+def check_result_for_error(sim_auto_output, message):
+    if sim_auto_output[0] != '':
+        print('Error: ' + sim_auto_output[0])
+    else:
+        print(message)
+        return sim_auto_output
+
+def create_pw_pool():
+    # Create 8 PowerWorld COM objects
+    pw.create_pw_pool()
+
+@pw.threaded
+def threaded_func(parameter, com_id=None, auto_sim=None):
+    print('%s: Starting new thread: %s' % (com_id, parameter))
+    # initializePWCase
+    check_result_for_error(auto_sim.OpenCase(filename), 'Case Open')
+    check_result_for_error(auto_sim.RunScriptCommand('EnterMode(RUN)'), 'Enter Mode RUN')
+
+    # Save state from before we switch
+    check_result_for_error(auto_sim.SaveState(), 'Save State')
+
+    # Run OPF
+    check_result_for_error(auto_sim.RunScriptCommand('SolvePrimalLP'), 'Solve Primal LP')
+
+    # getBranchState
+    change_status_field_array = VARIANT(pythoncom.VT_VARIANT | pythoncom.VT_ARRAY, ['busnum', 'busnum:1', 'LineCircuit', 'LineStatus'])
+    output_lines = check_result_for_error(auto_sim.GetParametersMultipleElement('Branch', change_status_field_array, ' '), 'Branch')
+    output_lines = np.array(output_lines[1]).T
+    output_flattened = output_lines.flatten()
+
+    # Store the results along with the COM id
+    return com_id, output_flattened
+
+def multiprocess():
+    results = threaded_func("foo")
+    while not results.empty():
+        result = results.get()
+        print(result)
+    pw.kill_com_objects()
+
+benchmarks = []
+if __name__ == '__main__':
+    benchmarks.append(timeit.Timer('create_pw_pool()', 'from __main__ import create_pw_pool').timeit(number=1))
+    benchmarks.append(timeit.Timer('multiprocess()', 'from __main__ import multiprocess').timeit(number=1))
+    print(benchmarks)
--- powergrid/powergrid.py
+++ powergrid/powergrid.py
@@ -1,130 +1,95 @@
-# -*- coding: utf-8 -*-
-
-import os
-import numpy as np
-import timeit
-
-# Python with COM requires the pyWin32 extensions
 import win32com.client
 from win32com.client import VARIANT
 
 import pythoncom
 from queue import Queue
-from threading import Thread
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-filename = current_dir + '\\resources\\sampleCase.pwb'
-pw_objects = Queue()
-max_objects = 2
+class PowerGrid:
+    def __init__(self, _num_threads):
+        self._num_threads = _num_threads
+        self.results = Queue()
+        self._pw_objects = Queue()
 
-# The following function will determine if any errors are returned and print an appropriate message.
-def check_result_for_error(sim_auto_output, message):
-    if sim_auto_output[0] != '':
-        print('Error: ' + sim_auto_output[0])
-    else:
-        print(message)
-        return sim_auto_output
+        # self._create_pw_pool(self.threads)
 
-results = []
-def log_result(result):
-    results.append(result)
+    def create_pw_pool(self):
+        for i in range(self._num_threads):
+            # Create COM object
+            pw = win32com.client.Dispatch('pwrworld.SimulatorAuto')
+            # Create stream that will hold COM object
+            pw_stream = pythoncom.CreateStreamOnHGlobal()
+            # Convert COM object into stream to allow re-usage
+            pythoncom.CoMarshalInterface(pw_stream,
+                                         pythoncom.IID_IDispatch,
+                                         pw._oleobj_,
+                                         pythoncom.MSHCTX_LOCAL,
+                                         pythoncom.MSHLFLAGS_TABLESTRONG)
+            # No need for the COM reference anymore now that it's a stream
+            pw = None
+            # Store the stream in a queue, along with the ID of the stream
+            self._pw_objects.put((i, pw_stream))
 
+    def threaded(self, f, daemon=False):
+        from threading import Thread
 
-def create_pw_pool():
-    for i in range(max_objects):
-        # Create COM object
-        pw = win32com.client.Dispatch('pwrworld.SimulatorAuto')
-        # Create stream that will hold COM object
-        pw_stream = pythoncom.CreateStreamOnHGlobal()
-        # Convert COM object into stream to allow re-usage
-        pythoncom.CoMarshalInterface(pw_stream,
-                                     pythoncom.IID_IDispatch,
-                                     pw._oleobj_,
-                                     pythoncom.MSHCTX_LOCAL,
-                                     pythoncom.MSHLFLAGS_TABLESTRONG)
-        # No need for the COM reference anymore now that it's a stream
-        pw = None
-        # Store the stream in a queue
-        pw_objects.put((i, pw_stream))
+        def threaded_f(results, *args, **kwargs):
+            # Enable COM object access in this thread, but not others
+            pythoncom.CoInitialize()
+            # Get tuple of id and stream
+            pw_data = self._pw_objects.get()
+            # Get the ID
+            pw_id = pw_data[0]
+            # Get stream reference from queue
+            pw_stream = pw_data[1]
+            # Make sure we're at the start of the stream, reset the pointer
+            pw_stream.Seek(0, 0)
+            # Unmarshal the stream, going back to the original interface
+            pw_interface = pythoncom.CoUnmarshalInterface(pw_stream, pythoncom.IID_IDispatch)
+            # And finally return the COM object that was created earlier
+            pw = win32com.client.Dispatch(pw_interface)
 
-def multiprocess(threads):
-    for i in range(threads):
-        worker = Thread(target=load_sample_case, args=(i, pw_objects,))
-        worker.start()
-    worker.join()
+            # Call the actual threaded function
+            com_result = f(*args, com_id=pw_id, auto_sim=pw, **kwargs)
+            # Return function results through Queue
+            results.put(com_result)
 
-    clean_pw_queue()
-    print('*** Done')
+            # Revert stream back to start position
+            pw_stream.Seek(0, 0)
+            # Return stream back to queue
+            self._pw_objects.put((pw_id, pw_stream))
+            # Indicate all work on this queue object is done. Without this the queue task counter would go up every time
+            # a stream is re-added to the queue
+            self._pw_objects.task_done()
+            # Clean up COM reference
+            pw = None
+            # Indicate that no more COM objects will be called in this thread
+            pythoncom.CoUninitialize()
 
+        def wrapped(*args, **kwargs):
+            results = Queue()
 
-def clean_pw_queue():
-    while not pw_objects.empty():
-        pw = pw_objects.get()[1]
-        pythoncom.CoReleaseMarshalData(pw)
-        pw = None
-        pw_objects.task_done()
-    pw_objects.mutex.acquire()
-    pw_objects.queue.clear()
-    pw_objects.all_tasks_done.notify_all()
-    pw_objects.unfinished_tasks = 0
-    pw_objects.mutex.release()
+            threads = [Thread(target=threaded_f, args=(results,)+args, kwargs=kwargs) for i in range(self._num_threads)]
+            for t in threads:
+                t.daemon = daemon
+            [t.start() for t in threads]
+            [t.join() for t in threads]
+            self.result_queue = results
 
+            return self.result_queue
 
-def load_sample_case(i, q):
-    print('%s: Starting new thread' % i)
-    # Enable COM object access in this thread, but not others
-    pythoncom.CoInitialize()
-    # Get tuple of id and stream
-    pw_data = q.get()
-    # Get the ID
-    pw_id = pw_data[0]
-    # Get stream reference from queue
-    pw_stream = pw_data[1]
-    # Make sure we're at the start of the stream, reset the pointer
-    pw_stream.Seek(0, 0)
-    # Unmarshal the stream, going back to the original interface
-    pw_interface = pythoncom.CoUnmarshalInterface(pw_stream, pythoncom.IID_IDispatch)
-    # And finally return the COM object that was created earlier
-    pw = win32com.client.Dispatch(pw_interface)
+        return wrapped
 
-    # initializePWCase
-    check_result_for_error(pw.OpenCase(filename), 'Case Open')
-    check_result_for_error(pw.RunScriptCommand('EnterMode(RUN)'), 'Enter Mode RUN')
-
-    # Save state from before we switch
-    check_result_for_error(pw.SaveState(), 'Save State')
-
-    # Run OPF
-    check_result_for_error(pw.RunScriptCommand('SolvePrimalLP'), 'Solve Primal LP')
-
-    # getBranchState
-    change_status_field_array = VARIANT(pythoncom.VT_VARIANT | pythoncom.VT_ARRAY, ['busnum', 'busnum:1', 'LineCircuit', 'LineStatus'])
-    output_lines = check_result_for_error(pw.GetParametersMultipleElement('Branch', change_status_field_array, ' '), 'Branch')
-    output_lines = np.array(output_lines[1]).T
-    output_flattened = output_lines.flatten()
-
-    log_result(output_flattened)
-
-    # Revert stream back to start position
-    pw_stream.Seek(0, 0)
-    # Return stream back to queue
-    q.put((pw_id, pw_stream))
-    # Indicate all work on this queue object is done. Without this the queue task counter would go up every time a
-    # stream is re-added to the queue
-    q.task_done()
-    # Clean up COM reference
-    pw = None
-    # Indicate that no more COM objects will be called in this thread
-    pythoncom.CoUninitialize()
-
-
-benchmarks = []
-
-if __name__ == '__main__':
-    benchmarks.append(timeit.Timer('create_pw_pool()', 'from __main__ import create_pw_pool').timeit(number=1))
-    # benchmarks.append(timeit.Timer('serial()', 'from __main__ import serial').timeit(number=1))
-    # benchmarks.append(timeit.Timer('multiprocess(1)', 'from __main__ import multiprocess').timeit(number=1))
-    benchmarks.append(timeit.Timer('multiprocess(4)', 'from __main__ import multiprocess').timeit(number=1))
-    # benchmarks.append(timeit.Timer('multiprocess(3)', 'from __main__ import multiprocess').timeit(number=1))
-    # benchmarks.append(timeit.Timer('multiprocess(4)', 'from __main__ import multiprocess').timeit(number=1))
-    print(benchmarks)
+    def kill_com_objects(self):
+        for i in range(self._num_threads):
+            pw = self._pw_objects.get()[1]
+            pythoncom.CoReleaseMarshalData(pw)
+            pw = None
+            self._pw_objects.task_done()
+        self._pw_objects = None
+        # The queue should be empty at this point, this is a fallback to clear the queue
+        # Doesn't kill COM objects though
+        # self._pw_objects.mutex.acquire()
+        # self._pw_objects.queue.clear()
+        # self._pw_objects.all_tasks_done.notify_all()
+        # self._pw_objects.unfinished_tasks = 0
+        # self._pw_objects.mutex.release()
