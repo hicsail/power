c2b3c25197f31adf9361ffbbe9a5e046a4817318
Frederick Jansen <frederick.jansen@gmail.com>
2015-10-13 17:39:31 -0400

    Changed main to implement new power interface

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import os
 import numpy as np
 import timeit
 
-from powergrid.powergrid import PowerGrid
+from power.power import Power
 # Python with COM requires the pyWin32 extensions
 from win32com.client import VARIANT
 
 import pythoncom
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-filename = current_dir + '\\powergrid\\resources\\sampleCase.pwb'
-pw = PowerGrid(8)
+filename = current_dir + '\\power\\resources\\sampleCase.pwb'
+pw = Power(8)
+
 
 # The following function will determine if any errors are returned and print an appropriate message.
 def check_result_for_error(sim_auto_output, message):
@@ -23,13 +24,14 @@ def check_result_for_error(sim_auto_output, message):
         print(message)
         return sim_auto_output
 
+
 def create_pw_pool():
     # Create 8 PowerWorld COM objects
     pw.create_pw_pool()
 
-@pw.threaded
-def threaded_func(parameter, com_id=None, auto_sim=None):
-    print('%s: Starting new thread: %s' % (com_id, parameter))
+
+def threaded_func(com_id=None, auto_sim=None):
+    print('Starting new thread: %s' % com_id)
     # initializePWCase
     check_result_for_error(auto_sim.OpenCase(filename), 'Case Open')
     check_result_for_error(auto_sim.RunScriptCommand('EnterMode(RUN)'), 'Enter Mode RUN')
@@ -47,14 +49,18 @@ def threaded_func(parameter, com_id=None, auto_sim=None):
     output_flattened = output_lines.flatten()
 
     # Store the results along with the COM id
-    return com_id, output_flattened
+    return output_flattened
+
+
+def callback(task, result):
+    print("**** Result from request #%s: %r" % (task.pw_id, result))
+
 
 def multiprocess():
-    results = threaded_func("foo")
-    while not results.empty():
-        result = results.get()
-        print(result)
-    pw.kill_com_objects()
+    pw.add_task(threaded_func, '0-7', callback)
+    pw.poll(block=True)
+    pw.reset()
+
 
 benchmarks = []
 if __name__ == '__main__':
