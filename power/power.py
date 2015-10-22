b601e62cf82822c710f9dd30754248b8ada2a1d2
Frederick Jansen <frederick.jansen@gmail.com>
2015-10-22 16:48:15 -0400

    Changed PW objects to be in list instead of queue, and added thread IDs instead of COM IDs Cleans up the code a bit, and fully synchronizes threads, tasks and com objects (though only the final two should by synchronized and that was already the case)

--- power/power.py
+++ power/power.py
@@ -11,13 +11,14 @@ from typing import Sequence, List, Callable
 
 class PowerThread(Thread):
     """A thread that runs indefinitely and uses a queue to obtain more tasks."""
-    def __init__(self, tasks: Sequence[Queue], results: Queue, pw_objects: Queue, **kwargs):
+    def __init__(self, i: int, task_list: Sequence[Queue], results: Queue, pw_objects: list, **kwargs):
         Thread.__init__(self, **kwargs)
         self.setDaemon(0)
+        self._thread_id = i
         self._results = results
         self._pw_objects = pw_objects
         self._tasks = tasks
-        self._pw_id, self._pw, self._pw_stream = None, None, None
+        self._pw, self._pw_stream = None, None
         self._dismissed = False
         self.start()
 
@@ -28,12 +29,8 @@ class PowerThread(Thread):
         """
         # Enable COM object access in this thread, but not others
         pythoncom.CoInitialize()
-        # Get tuple of id and stream
-        pw_data = self._pw_objects.get()
-        # Get the ID
-        pw_id = pw_data[0]
-        # Get stream reference from queue
-        pw_stream = pw_data[1]
+        # Get stream reference from list of com objects
+        pw_stream = self._pw_objects[self._thread_id]
         # Make sure we're at the start of the stream, reset the pointer
         pw_stream.Seek(0, 0)
         # Unmarshal the stream, going back to the original interface
@@ -41,17 +38,14 @@ class PowerThread(Thread):
         # And finally return the COM object that was created earlier
         pw = win32com.client.Dispatch(pw_interface)
 
-        return pw_id, pw, pw_stream
+        return pw, pw_stream
 
     def unmarshal_com(self):
         """Unmarshal the PowerWorld COM object and return it to the queue for later usage."""
         # Revert stream back to start position
         self._pw_stream.Seek(0, 0)
         # Return stream back to queue
-        self._pw_objects.put((self._pw_id, self._pw_stream))
-        # Indicate all work on this queue object is done. Without this the queue task counter would go up every time
-        # a stream is re-added to the queue
-        self._pw_objects.task_done()
+        self._pw_objects[self._thread_id] = self._pw_stream
         # Clean up COM reference
         self._pw = None
         # Indicate that no more COM objects will be called in this thread
@@ -60,19 +54,19 @@ class PowerThread(Thread):
     def run(self):
         """Continuously run thread, consuming new tasks as we go."""
         # Can't do this before the thread starts running, otherwise marshalling is not successful
-        self._pw_id, self._pw, self._pw_stream = self.marshal_com()
+        self._pw, self._pw_stream = self.marshal_com()
         while True:
             if self._dismissed:
                 break
             try:
                 # Get task with non-blocking Queue call
-                task = self._tasks[self._pw_id].get(False)
+                task = self._tasks.get(False)
             except queue.Empty:
                 continue
             else:
                 try:
                     # Call task function and store results
-                    result = task.f(*task.args, com_id=self._pw_id, auto_sim=self._pw, **task.kwargs)
+                    result = task.f(*task.args, thread_id=self._thread_id, auto_sim=self._pw, **task.kwargs)
                     self._results.put((task, result))
                 except:
                     # Or store exception message if something went wrong
@@ -84,9 +78,9 @@ class PowerThread(Thread):
 
 
 class PowerTask:
-    def __init__(self, f, pw_id, *args, **kwargs):
+    def __init__(self, f, thread_id, *args, **kwargs):
         self.f = f
-        self.pw_id = pw_id
+        self.thread_id = thread_id
         self.args = args
         self.kwargs = kwargs
 
@@ -94,7 +88,7 @@ class PowerTask:
 class Power:
     """
     :type _num_threads: int
-    :type _pw_objects: Queue
+    :type _pw_objects: list
     :type _threads: list[PowerThread]
     :type _dismissed_threads: list[PowerThread]
     :type _tasks: list[Queue]
@@ -102,7 +96,7 @@ class Power:
     """
     def __init__(self, _num_threads):
         self._num_threads = _num_threads
-        self._pw_objects = Queue()
+        self._pw_objects = []
         self._threads = []
         self._dismissed_threads = []
         self._tasks = [Queue() for _ in range(_num_threads)]
@@ -122,11 +116,11 @@ class Power:
                                          pythoncom.MSHLFLAGS_TABLESTRONG)
             # No need for the COM reference anymore now that it's a stream
             pw = None
-            # Store the stream in a queue, along with the ID of the stream
-            self._pw_objects.put((i, pw_stream))
+            # Store the stream in a list
+            self._pw_objects.append(pw_stream)
 
         for i in range(self._num_threads):
-            self._threads.append(PowerThread(self._tasks, self._results, self._pw_objects))
+            self._threads.append(PowerThread(i, self._tasks, self._results, self._pw_objects))
 
     def add_task(self, f: Callable, threads: str, *args, **kwargs):
         for i in self._parse_thread_list(threads):
@@ -145,13 +139,12 @@ class Power:
     def reset(self):
         for i in range(self._num_threads):
             # Clean COM objects
-            pw = self._pw_objects.get()[1]
+            pw = self._pw_objects[i]
             pythoncom.CoReleaseMarshalData(pw)
             pw = None
-            self._pw_objects.task_done()
             # Clean task queue
-            self.tasks[i].queue.clear()
-            self.tasks[i].all_tasks_done.notify_all()
+            self._tasks[i].queue.clear()
+            self._tasks[i].all_tasks_done.notify_all()
         self._pw_objects = None
         self._tasks = None
         # TODO join threads first?
